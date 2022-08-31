fb13822f624024478eff71263bd82560df39da92
Rahisul H <rahisul@icloud.com>
2022-08-31 12:36:51 -0500

    adding ex4

--- ex4.py
+++ ex4.py
@@ -6,7 +6,7 @@
 #    By: rahisulhaque <rahisul@icloud.com>              / ___/ __ `/ __ \/ / ___/ / / / /     #
 #                                                      / /  / /_/ / / / / (__  ) /_/ / /      #
 #    Created: 2022/07/25 12:03:52 by rahisulhaque     /_/   \__,_/_/ /_/_/____/___,_/_/       #
-#    Updated: 2022/07/25 12:29:41 by rahisulhaque                                             #
+#    Updated: 2022/08/31 12:29:56 by rahisulhaque                                             #
 #                                                                                             #
 # ******************************************************************************************* #
 
@@ -17,4 +17,11 @@ passengers = 90
 cars_not_driven = cars - drivers
 cars_driven = drivers
 carpool_capacity = cars_driven * space_in_a_car
-average_passenger_per_car = passenger / cars_driven
+average_passengers_per_car = passengers / cars_driven
+
+print("There are ",cars, " cars available.")
+print("There are only ",drivers," driver available.")
+print("There will be ", cars_not_driven," empty cars today.")
+print("We can transport ", carpool_capacity," people today.")
+print("We have ", passengers, " to carpool today.")
+print("We need to put about ", average_passengers_per_car," in each car.")
