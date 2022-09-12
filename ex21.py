e103cf01e6ca29b29af5c68d20c34433aed24a42
Rahisul H <rahisul@icloud.com>
2022-09-12 07:26:11 -0500

    adding 19, 20 and 21

---
+++ ex21.py
@@ -0,0 +1,52 @@
+# ******************************************************************************************* #
+#                                                                                             #
+#                                                                                             #
+#    ex21.py                                                         __    _            __    #
+#                                                        _________ _/ /_  (_)______  __/ /    #
+#    By: rahisulhaque <rahisul@icloud.com>              / ___/ __ `/ __ \/ / ___/ / / / /     #
+#                                                      / /  / /_/ / / / / (__  ) /_/ / /      #
+#    Created: 2022/09/11 11:02:37 by rahisulhaque     /_/   \__,_/_/ /_/_/____/___,_/_/       #
+#    Updated: 2022/09/11 11:44:57 by rahisulhaque                                             #
+#                                                                                             #
+# ******************************************************************************************* #
+
+def add(a, b):
+	print(f"ADDING {a} + {b}")
+	return a+b
+
+def subtract(a, b):
+	print(f"SUBTRACTING {a} - {b}")
+	return a-b
+
+
+def multiply(a, b):
+	print(f"MULTIPLYING {a} * {b}")
+	return a*B
+
+def divide(a, b)
+	print(f"DIVICING {a} / {b}")
+	rerturn a/b
+
+print("Let's do some math with just fucntions!")
+
+age = add(20,30)
+height = subtract(70,20)
+weight = multiply(5,10)
+iq = devide(100,2)
+
+print(f"Age: {age},\n Height: {height}, \n Weight: {weight, \n IQ:{iq}")
+
+#puzzle 
+
+print("Here is a puzzle:")
+
+what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
+
+print("That becomes:", what, "Can you do it by hand?")
+
+
+
+
+
+
+
